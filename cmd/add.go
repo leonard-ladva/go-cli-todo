5743c7794a87f7d03ba9c78c14e4f6d92e39b36e
Leonard <leonard.ladva@gmail.com>
2025-11-12 13:04:58 +0200

    starting point with cobra and add todo functionality

---
+++ cmd/add.go
@@ -0,0 +1,62 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var addCmd = &cobra.Command{
+	Use: "add [todo]",
+	Short: "Add a task",
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		filePath := "file.json"
+		todo := args[0]
+
+		var todos []string
+
+		data, err := os.ReadFile(filePath)
+		if err != nil {
+			fmt.Printf("Error reading from file %v\n", err)
+			os.Exit(2)
+		}
+
+		err = json.Unmarshal(data, &todos)
+		if err != nil {
+			fmt.Printf("Error converting from json %v\n", err)
+			os.Exit(2)
+		}
+
+		todos = append(todos, todo)
+
+		newData, err := json.MarshalIndent(todos, "", " ")
+		if err != nil {
+			fmt.Printf("Error converting to json %v\n", err)
+			os.Exit(2)
+		}
+		err = os.WriteFile(filePath, newData, 0644)
+		if err != nil {
+			fmt.Printf("Error converting to json %v\n", err)
+			os.Exit(2)
+		}
+		// file, err := os.OpenFile(filePath, os.O_APPEND | os.O_CREATE | os.O_WRONLY, 0644)
+		// if err != nil {
+		// 	fmt.Printf("Error opening file %v\n", err)
+		// 	os.Exit(2)
+		// }
+		// defer file.Close()
+
+		// if _, err = file.WriteString(todo); err != nil {
+		// 	fmt.Printf("Error writing to file %v\n", err)
+		// 	os.Exit(2)
+		// }
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(addCmd)
+}
